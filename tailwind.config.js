47285368c15c620f2592008195f53eb277c3954e
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-10-14 09:50:47 +0200

    Modification des noms de couleurs et des tailles de police dans la configuration de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        'Light Brown': '#ddc99c',
+        LightBrown: '#ddc99c',
         Brown: '#654930',
-        'Drak Brown': '#3f2a13',
+        DarkBrown: '#3f2a13',
         Fond1: '#ddc99c',
         White: '#fff8ea'
       },
@@ -16,20 +16,12 @@ export default {
         base: '1rem',
         lg: '1.125rem',
         xl: '1.5rem',
-        '2xl': '2rem'
+        xxl: '2rem'
       },
       fontFamily: {
         rowdies: 'Rowdies',
         itim: 'Itim',
         biryani: 'Biryani'
-      },
-      borderRadius: {
-        'rounded-0': '0rem',
-        'rounded-1': '0.3125rem',
-        'rounded-2': '0.5rem',
-        'rounded-3': '0.78125rem',
-        'rounded-4': '1.875rem',
-        'rounded-5': '62.4375rem'
       }
     }
   },
