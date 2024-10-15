f335ea335914e4371f063b6e8cb2c89b0440c4b6
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-10-15 16:08:49 +0200

    Ajout de la route '/legalmention' et création du fichier legalmention.vue

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -21,6 +21,8 @@ declare module 'vue-router/auto-routes' {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
     '/guidestyle': RouteRecordInfo<'/guidestyle', '/guidestyle', Record<never, never>, Record<never, never>>,
+    '/legalmention': RouteRecordInfo<'/legalmention', '/legalmention', Record<never, never>, Record<never, never>>,
+    '/legalMention': RouteRecordInfo<'/legalMention', '/legalMention', Record<never, never>, Record<never, never>>,
     '/projectSolo/': RouteRecordInfo<'/projectSolo/', '/projectSolo', Record<never, never>, Record<never, never>>,
     '/projectSolo/[id]': RouteRecordInfo<'/projectSolo/[id]', '/projectSolo/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
     '/realisations': RouteRecordInfo<'/realisations', '/realisations', Record<never, never>, Record<never, never>>,
