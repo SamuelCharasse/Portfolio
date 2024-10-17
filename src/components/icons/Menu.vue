f073545d7340d35828d1b5e6b62ecf5c0e00f612
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-10-17 11:02:03 +0200

    Refactor SVG templates in icon components for improved readability

--- src/components/icons/Menu.vue
+++ src/components/icons/Menu.vue
@@ -1,27 +1,68 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template><svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_dd_175_9)">
-<rect x="8" y="4" width="48" height="48" rx="8" fill="#DDC99C" shape-rendering="crispEdges"/>
-<rect x="6" y="2" width="52" height="52" rx="10" stroke="#654930" stroke-width="4" shape-rendering="crispEdges"/>
-<path d="M38 28H28M38 20H28M46 38V14C46 12.9391 45.5786 11.9217 44.8284 11.1716C44.0783 10.4214 43.0609 10 42 10H16" stroke="#654930" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M24 46H48C49.0609 46 50.0783 45.5786 50.8284 44.8284C51.5786 44.0783 52 43.0609 52 42V40C52 39.4696 51.7893 38.9609 51.4142 38.5858C51.0391 38.2107 50.5304 38 50 38H30C29.4696 38 28.9609 38.2107 28.5858 38.5858C28.2107 38.9609 28 39.4696 28 40V42C28 43.0609 27.5786 44.0783 26.8284 44.8284C26.0783 45.5786 25.0609 46 24 46ZM24 46C22.9391 46 21.9217 45.5786 21.1716 44.8284C20.4214 44.0783 20 43.0609 20 42V14C20 12.9391 19.5786 11.9217 18.8284 11.1716C18.0783 10.4214 17.0609 10 16 10C14.9391 10 13.9217 10.4214 13.1716 11.1716C12.4214 11.9217 12 12.9391 12 14V18C12 18.5304 12.2107 19.0391 12.5858 19.4142C12.9609 19.7893 13.4696 20 14 20H20" stroke="#654930" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-</g>
-<defs>
-<filter id="filter0_dd_175_9" x="0" y="0" width="64" height="64" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="4"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_175_9"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="4"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-<feBlend mode="normal" in2="effect1_dropShadow_175_9" result="effect2_dropShadow_175_9"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_175_9" result="shape"/>
-</filter>
-</defs>
-</svg></template>
+<template>
+  <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_dd_175_9)">
+      <rect x="8" y="4" width="48" height="48" rx="8" fill="#DDC99C" shape-rendering="crispEdges" />
+      <rect
+        x="6"
+        y="2"
+        width="52"
+        height="52"
+        rx="10"
+        stroke="#654930"
+        stroke-width="4"
+        shape-rendering="crispEdges"
+      />
+      <path
+        d="M38 28H28M38 20H28M46 38V14C46 12.9391 45.5786 11.9217 44.8284 11.1716C44.0783 10.4214 43.0609 10 42 10H16"
+        stroke="#654930"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 46H48C49.0609 46 50.0783 45.5786 50.8284 44.8284C51.5786 44.0783 52 43.0609 52 42V40C52 39.4696 51.7893 38.9609 51.4142 38.5858C51.0391 38.2107 50.5304 38 50 38H30C29.4696 38 28.9609 38.2107 28.5858 38.5858C28.2107 38.9609 28 39.4696 28 40V42C28 43.0609 27.5786 44.0783 26.8284 44.8284C26.0783 45.5786 25.0609 46 24 46ZM24 46C22.9391 46 21.9217 45.5786 21.1716 44.8284C20.4214 44.0783 20 43.0609 20 42V14C20 12.9391 19.5786 11.9217 18.8284 11.1716C18.0783 10.4214 17.0609 10 16 10C14.9391 10 13.9217 10.4214 13.1716 11.1716C12.4214 11.9217 12 12.9391 12 14V18C12 18.5304 12.2107 19.0391 12.5858 19.4142C12.9609 19.7893 13.4696 20 14 20H20"
+        stroke="#654930"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_dd_175_9"
+        x="0"
+        y="0"
+        width="64"
+        height="64"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_175_9" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="effect1_dropShadow_175_9" result="effect2_dropShadow_175_9" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_175_9" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+</template>
