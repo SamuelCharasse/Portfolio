d9d1c84f69b14d55cc433e15cff6a931111a4b77
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-10-18 14:15:01 +0200

    Ajout de la fonctionnalité de basculement du son avec les icônes SoundOn et SoundOff, mise à jour des propriétés de style dans soundOff.vue.

--- src/pages/guidestyle.vue
+++ src/pages/guidestyle.vue
@@ -5,6 +5,14 @@ import SoundOff from '@/components/icons/soundOff.vue'
 import SoundOn from '@/components/icons/soundOn.vue'
 import MenuOverlay from '@/components/MenuOverlay.vue'
 import Tile from '@/components/Tile.vue'
+import { ref } from 'vue'
+
+const isSoundOn = ref(true);
+
+const toggleSound = () => {
+  isSoundOn.value = !isSoundOn.value;
+};
+
 </script>
 <template>
   <div class="flex flex-col gap-4">
@@ -12,7 +20,7 @@ import Tile from '@/components/Tile.vue'
     <BtnDefault>Envoyer un message</BtnDefault>
     <MenuOverlay />
     <Tile></Tile>
-    <SoundOn></SoundOn>
-    <SoundOff></SoundOff>
+    <SoundOn v-if="isSoundOn" @click="toggleSound"></SoundOn>
+    <SoundOff v-else @click="toggleSound"></SoundOff>
   </div>
 </template>
