4a539d5bd2c36c772677039012b79e4af3716176
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-11-15 09:27:52 +0100

    Ajout de nouveaux composants SVG pour les séparateurs et mise à jour de la mise en page de la page Project Solo.

--- src/pages/projectSolo/index.vue
+++ src/pages/projectSolo/index.vue
@@ -1,4 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <h1>Project Solo</h1>
+  <div class="-z-10 fixed">
+    <img src="/src/assets/ScrollBackground2.webp" alt="" />
+  </div>
+  <main class="p-32 h-screen">
+    <div class="bg-LightBrown w-full h-full p-2 gap-4">
+
+    </div>
+  </main>
 </template>
