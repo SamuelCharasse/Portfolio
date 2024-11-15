e7113b24263d570be97d86e54f6115503db679fb
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-11-15 10:18:50 +0100

    CSS update

--- src/pages/projectSolo/index.vue
+++ src/pages/projectSolo/index.vue
@@ -1,11 +1,16 @@
+<script setup lang="ts">
+import Separateur1 from '@/components/icons/separateur1.vue';
+</script>
+
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="-z-10 fixed">
     <img src="/src/assets/ScrollBackground2.webp" alt="" />
   </div>
   <main class="p-32 h-screen">
-    <div class="bg-LightBrown w-full h-full p-2 gap-4">
-
+    <div class="flex flex-col bg-LightBrown w-full h-full p-2 gap-4 items-center">
+        <Separateur1 />
+        <h2>Idralwel - Tales of Aeshan</h2>
     </div>
   </main>
 </template>
