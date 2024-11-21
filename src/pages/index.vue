dc44d407ae265f38e54e141d63e6c02e200e9e08
Samuel CHARASSE <119793868+SamuelCharasse@users.noreply.github.com>
2024-11-21 13:15:42 +0100

    Ajout de la fonctionnalité de menu superposé et de contrôle du son, mise à jour de la logique de gestion des événements dans index.vue.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,44 +1,88 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { RouterView, RouterLink } from 'vue-router';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { useRouter } from 'vue-router';
 import Menu from '@/components/icons/Menu.vue';
 import Tile from '@/components/Tile.vue';
 import Popup from '@/components/Popup.vue';
+import MenuOverlay from '@/components/MenuOverlay.vue';
+import SoundOn from '@/components/icons/SoundOn.vue';
+import SoundOff from '@/components/icons/SoundOff.vue';
 
 const showPopup = ref(false);
+const showOverlayMenu = ref(false);
+const isSoundOn = ref(true);
+const router = useRouter();
 
 onMounted(() => {
   if (!localStorage.getItem('hasVisited')) {
     showPopup.value = true;
     localStorage.setItem('hasVisited', 'true');
   }
+
+  document.addEventListener('click', handleClickOutside);
+  router.afterEach(() => {
+    showOverlayMenu.value = false;
+  });
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClickOutside);
 });
 
 const closePopup = () => {
   showPopup.value = false;
 };
+
+const toggleOverlayMenu = () => {
+  showOverlayMenu.value = !showOverlayMenu.value;
+};
+
+const toggleSound = () => {
+  isSoundOn.value = !isSoundOn.value;
+};
+
+const handleClickOutside = (event: MouseEvent) => {
+  const overlayMenuElement = document.querySelector('.overlay-menu');
+  const menuIconElement = document.querySelector('.menu-icon');
+  if (
+    overlayMenuElement &&
+    !overlayMenuElement.contains(event.target as Node) &&
+    menuIconElement &&
+    !menuIconElement.contains(event.target as Node)
+  ) {
+    showOverlayMenu.value = false;
+  }
+};
 </script>
 
 <template>
-  <main class="relative flex justify-center items-center min-h-screen">
-    <img 
-      src="/src/assets/Portfolio Sam V2.webp" 
+  <main class="relative flex min-h-screen items-center justify-center">
+    <img
+      src="/src/assets/Portfolio Sam V2.webp"
       alt="Cartographie d'un continent dans un style fantastique avec un effet de parchemin"
-      class="absolute inset-0 w-full h-auto max-w-none object-cover z-0">
-    
+      class="absolute inset-0 z-0 h-auto w-full max-w-none object-cover"
+    />
+
     <!-- Menu positionné -->
-    <div class="absolute z-10 top-[5%] left-[2.5%]">
-      <Menu/>
+    <div class="absolute left-[2.5%] top-[5%] z-10">
+      <Menu @click="toggleOverlayMenu" class="menu-icon" />
+      <!-- Menu overlay -->
+      <MenuOverlay class="mt-2 overlay-menu" v-if="showOverlayMenu" />
     </div>
     <div class="absolute">
       <Tile>
         <RouterLink to="/">Test</RouterLink>
       </Tile>
     </div>
-    <RouterView class="relative z-10"/>
-    
+    <div class="absolute bottom-[5%] right-[2.5%] z-10">
+      <SoundOn v-if="isSoundOn" @click="toggleSound"></SoundOn>
+      <SoundOff v-else @click="toggleSound"></SoundOff>
+    </div>
+
+    <RouterView class="relative z-10" />
+
     <!-- Pop-up -->
-    <Popup v-if="showPopup" @close="closePopup"/>
+    <Popup v-if="showPopup" @close="closePopup" />
   </main>
 </template>
