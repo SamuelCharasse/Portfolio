6da4269110b2cb36505c8f2d2ef0e86913133cca
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-12-06 15:07:49 +0100

    Ajout de la fonctionnalité plein écran au composant ImageTemplate; implémentation d'un toggle pour activer/désactiver le mode plein écran lors du clic sur l'image

--- src/components/ImageTemplate.vue
+++ src/components/ImageTemplate.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { defineProps } from 'vue'
 
 defineProps({
@@ -11,11 +12,27 @@ defineProps({
     required: true
   }
 })
+
+const isFullScreen = ref(false)
+
+const toggleFullScreen = () => {
+  isFullScreen.value = !isFullScreen.value
+}
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 w-fit">
-    <img :src="imageUrl" alt="Image" class="h-[250px] object-fill stroke-Brown stroke-1 rounded-lg shadow-4" />
+  <div class="flex w-fit flex-col gap-2">
+    <img
+      :src="imageUrl"
+      alt="Image"
+      @click="toggleFullScreen"
+      :class="[
+        'shadow-4 h-[250px] w-full transform rounded-lg stroke-Brown stroke-1 object-fill transition-transform duration-300 ease-in-out hover:scale-110',
+        isFullScreen
+          ? 'max-w-screen z-50 h-auto max-h-screen w-auto cursor-pointer'
+          : 'cursor-pointer'
+      ]"
+    />
     <p class="mt-2 text-lightText">{{ caption }}</p>
   </div>
 </template>
