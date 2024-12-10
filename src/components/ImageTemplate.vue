ab3dc98a9e6956dfe0a4acaff0cbfcc399a72889
Samuel CHARASSE <119793868+SamuelCharasse@users.noreply.github.com>
2024-12-10 18:59:51 +0100

    Ajout d'un overlay pour le mode plein écran dans ImageTemplate.vue; amélioration de l'interaction avec l'image

--- src/components/ImageTemplate.vue
+++ src/components/ImageTemplate.vue
@@ -22,16 +22,24 @@ const toggleFullScreen = () => {
 
 <template>
   <div class="flex w-fit flex-col gap-2">
+    <!-- Overlay pour le mode plein écran -->
+    <div v-if="isFullScreen" 
+         class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+         @click="toggleFullScreen">
+      <img
+        :src="imageUrl"
+        alt="Image"
+        class="max-h-[90vh] max-w-[90vw] rounded-lg object-contain cursor-pointer"
+      />
+    </div>
+
+    <!-- Image normale -->
     <img
+      v-else
       :src="imageUrl"
       alt="Image"
       @click="toggleFullScreen"
-      :class="[
-        'shadow-4 h-[250px] w-full transform rounded-lg stroke-Brown stroke-1 object-fill transition-transform duration-300 ease-in-out hover:scale-110',
-        isFullScreen
-          ? 'max-w-screen z-50 h-auto max-h-screen w-auto cursor-pointer'
-          : 'cursor-pointer'
-      ]"
+      class="h-[250px] w-full rounded-lg object-fill shadow-lg transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer"
     />
     <p class="mt-2 text-lightText">{{ caption }}</p>
   </div>
