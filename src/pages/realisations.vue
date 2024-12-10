b907b21d4f663d689911dd4143700775d31145f1
Samuel CHARASSE <119793868+SamuelCharasse@users.noreply.github.com>
2024-12-10 21:53:57 +0100

    Ajout de la page "Réalisations" dans realizations.vue avec intégration de composants et contenu descriptif des projets

--- src/pages/realisations.vue
+++ src/pages/realisations.vue
@@ -1,1 +1,50 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import Separateur1 from '@/components/icons/separateur1.vue'
+import Separateur2 from '@/components/icons/separateur2.vue'
+import separateur3 from '@/components/icons/separateur3.vue'
+import btnDefault from '@/components/boutons/btnDefault.vue'
+import MenuAncre from '@/components/MenuAncre.vue'
+</script>
+<template>
+  <div class="flex h-full flex-col gap-8 bg-[url(/ScrollBackground2.webp)] p-32">
+    <MenuAncre :items="[]" />
+    <div class="flex h-fit w-full flex-col items-center gap-8 bg-LightBrown py-8">
+      <Separateur1 />
+      <h2>Réalisations</h2>
+      <Separateur2 />
+      <section class="flex p-32 gap-8">
+        <div class="flex flex-col gap-6 items-center w-fit">
+          <h3>Abyssal Wave</h3>
+          <p>
+            “Abyssal Wave” ou “Vague Abyssal” est un projet de premier semestre. Nous devions
+            réaliser un site mobile qui avait pour thème : “Réplique”. Site uniquement en version
+            mobile !
+          </p>
+          <btnDefault>Lien vers Abyssal Wave</btnDefault>
+        </div>
+        <div class="flex flex-col gap-6 items-center w-fit">
+          <h3>Manteco</h3>
+          <p>
+            Mantéco est une initiative à l’origine du Pôle Nord Franche-Comté.Le projet est porté
+            par une petite équipe de 4 personnes, tous travaillant en Nord Franche-Comté. Mantéco
+            souhaite informer les Nord Francs-Comtois et leur proposer des solutions pour une
+            mobilité plus douce, et respectueuse de l’environnement.
+          </p>
+          <btnDefault><a href="https://manteco.scharasse.fr/" target="_blank">Lien vers Manteco</a></btnDefault>
+        </div>
+        <div class="flex flex-col gap-6 items-center w-fit">
+          <h3>Optima</h3>
+          <p>
+            Optima est un site promotionnel fictif d’une exposition d’architecture. Il a été conçu
+            durant un examen de trois jours, maquette et intégration comprises. Site uniquement en
+            version mobile !
+          </p>
+          <btnDefault><a href="https://sae105.scharasse.fr/" target="_blank">Lien vers Optima</a></btnDefault>
+        </div>
+      </section>
+      <separateur3 />
+      <h2>À suivre...</h2>
+    </div>
+  </div>
+</template>
