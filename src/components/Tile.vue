c0bca081f8d6c019d42e550d7e6e70136ba75367
Samuel CHARASSE <119793868+SamuelCharasse@users.noreply.github.com>
2024-12-10 22:13:36 +0100

    Ajout d'effets de survol et de transition au composant Tile pour améliorer l'interaction utilisateur

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -4,7 +4,7 @@ import TileSection from '@/components/TileSection.vue'
 </script>
 
 <template>
-  <div class="relative flex w-max items-center">
+  <div class="relative flex w-max items-center hover:scale-125 transition-all duration-300">
     <TileSection class="w-full" />
     <h3 class="absolute left-0 right-0 top-4 flex items-center justify-center">
       <slot>Projets personnels</slot>
