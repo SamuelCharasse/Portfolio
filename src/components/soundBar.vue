907ff7548840cf9cab1d9447a5a241c08f36999b
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-12-13 10:52:24 +0100

    Amélioration de l'interface du composant SoundBar avec un nouveau style et simplification des contrôles de son

--- src/components/soundBar.vue
+++ src/components/soundBar.vue
@@ -1,49 +1,54 @@
 <template>
-  <div>
-    <button @click="playSound">Play Sound</button>
-    <button @click="pauseSound">Pause Sound</button>
-    <button @click="stopSound">Stop Sound</button>
-    <div>
+  <div class="flex flex-col gap-2 rounded-lg bg-LightBrown p-4 shadow-md border-Brown border-2">
+    <div class="flex justify-between items-center">
+      <button @click="playSound">Play</button>
+      <button @click="pauseSound">Pause</button>
+    </div>
+    <div class="flex flex-col">
       <label for="volume">Volume: {{ volume }}</label>
-      <input type="range" id="volume" min="0" max="1" step="0.01" v-model="volume" @input="adjustVolume" />
+      <input
+        type="range"
+        id="volume"
+        min="0"
+        max="1"
+        step="0.01"
+        v-model="volume"
+        @input="adjustVolume"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Howl, Howler } from 'howler';
+import { ref } from 'vue'
+import { Howl, Howler } from 'howler'
 
 // Créer une instance de Howl
 const soundHome = new Howl({
   src: ['/sounds/SoundIndex.mp3'],
   volume: 0.3,
   loop: true
-});
+})
 
 // Volume réactif
-const volume = ref(0.1);
+const volume = ref(0.1)
 
 // Méthodes pour contrôler le son
 const playSound = () => {
-    if (!soundHome.playing()) {
-      soundHome.play();
-    }
-};
+  if (!soundHome.playing()) {
+    soundHome.play()
+  }
+}
 
 const pauseSound = () => {
-  soundHome.pause();
-};
-
-const stopSound = () => {
-  soundHome.stop();
-};
+  soundHome.pause()
+}
 
 // Méthode pour ajuster le volume général
 const adjustVolume = () => {
-  Howler.volume(volume.value);
-};
+  Howler.volume(volume.value)
+}
 
 // Initialiser le volume général
-Howler.volume(volume.value);
+Howler.volume(volume.value)
 </script>
