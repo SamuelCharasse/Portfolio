dd21accc4034835b3ac33ec1660022237e4954e8
Samuel CHARASSE <119793868+SamuelCharasse@users.noreply.github.com>
2024-12-15 20:07:03 +0100

    Suppression de la condition pour la classe 'self-center' dans SkillAbout.vue

--- src/components/SkillAbout.vue
+++ src/components/SkillAbout.vue
@@ -19,7 +19,7 @@ const img = pb.getFileUrl(props.data, props.data.img)
   <div
     class="flex flex-col space-y-4 rounded-lg border-2 border-DarkBrown bg-White p-4 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
   >
-    <h4 :class="{ 'self-center': props.type === 'tool' }">{{ name }}</h4>
+    <h4 class="self-center">{{ name }}</h4>
     <img :src="img" :alt="name" class="self-center w-16 h-16 object-contain" />
   </div>
 </template>
